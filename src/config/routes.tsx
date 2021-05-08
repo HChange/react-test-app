f3d2ff9c8ab62760f2375023dedcb95a97e02b62
Change <chensj@hua-cloud.com.cn>
2021-05-08 18:18:05 +0800

    feat: 🎸 eslint & prettier comfiguration complete

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import {RouteConfig} from 'react-router-config';
-import About from 'src/pages/About'
-import Markdown from 'src/pages/markdown'
+import { RouteConfig } from 'react-router-config';
+import About from 'src/pages/About';
+import Markdown from 'src/pages/markdown';
 import MarkdownIt from 'src/pages/markdown-it';
-import RecoilTest from 'src/pages/recoil/index'
+import RecoilTest from 'src/pages/recoil/index';
 
-const routes:RouteConfig[] =[
-    {
-        path:'/',
-        exact:true,
-        render:()=><Redirect to={{pathname:'/about'}}/>
-    },
-    {
-        path:'/about',
-        component:About,
-        name:'关于',
-    },
-    {
-        path:'/markdown',
-        component:Markdown,
-        name:'Markdown'
-    },
-    {
-        path:'/markdown-it',
-        component:MarkdownIt,
-        name:'MarkdownIt'
-    },
-    {
-        path:'/recoil',
-        component:RecoilTest,
-        name:'recoil'
-    },
-] 
+const routes: RouteConfig[] = [
+	{
+		path: '/',
+		exact: true,
+		render: () => <Redirect to={{ pathname: '/about' }} />
+	},
+	{
+		path: '/about',
+		component: About,
+		name: '关于'
+	},
+	{
+		path: '/markdown',
+		component: Markdown,
+		name: 'Markdown'
+	},
+	{
+		path: '/markdown-it',
+		component: MarkdownIt,
+		name: 'MarkdownIt'
+	},
+	{
+		path: '/recoil',
+		component: RecoilTest,
+		name: 'recoil'
+	}
+];
 
 export default routes;
