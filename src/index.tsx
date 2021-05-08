8ef9062f923e0510628d81a83a39b3afef253c24
Change <chensj@hua-cloud.com.cn>
2021-05-08 19:56:23 +0800

    test: test

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-console.log('xx')
+console.log('xassx')
 
 ReactDOM.render(
 	<React.StrictMode>
