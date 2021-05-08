5f0000159d610a8e92f46ba8646601e8cc5e2344
Change <chensj@hua-cloud.com.cn>
2021-05-08 18:11:20 +0800

    feat: 🎸 init

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from 'react';
 import { renderRoutes } from 'react-router-config';
 
-const About = (props:any) => {
-    console.log(props);
-    
-    return (
-        <>
-        <div style={{width:'100%',height:'100%',display:'flex',justifyContent:'center',alignItems:'center'}}>
-            开发中...
-        </div>
-        {renderRoutes(props.route.routes)}
-        </>
-    )
-}
+const About = (props: any) => {
+	console.log(props);
 
-export default About
+	return (
+		<>
+			<div
+				style={{
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center'
+				}}
+			>
+				开发中...
+			</div>
+			{renderRoutes(props.route.routes)}
+		</>
+	);
+};
+
+export default About;
