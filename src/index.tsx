3a02b413320163d5641f28eea19bbf6098b707bd
Change <chensj@hua-cloud.com.cn>
2021-05-08 20:00:25 +0800

    test: 💍 test

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-console.log('cvves');
+console.log('cvxxxxxves');
 
 ReactDOM.render(
 	<React.StrictMode>
