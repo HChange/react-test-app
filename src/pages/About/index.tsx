37b4a15680696d84a6b0222998fcf2849d5f095d
Change <chensj@hua-cloud.com.cn>
2021-05-11 19:23:14 +0800

    feat: 🎸 架子搭建

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,24 +2,22 @@ import React from 'react';
 import { renderRoutes } from 'react-router-config';
 
 const About = (props: any) => {
-	console.log(props);
-
-	return (
-		<>
-			<div
-				style={{
-					width: '100%',
-					height: '100%',
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center'
-				}}
-			>
-				开发中...
-			</div>
-			{renderRoutes(props.route.routes)}
-		</>
-	);
+  return (
+    <>
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        开发中...
+      </div>
+      {renderRoutes(props.route.routes)}
+    </>
+  );
 };
 
 export default About;
