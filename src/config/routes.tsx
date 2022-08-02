ded9217b05fbbc904f4ddc27cc695cf10fdad3f1
Change <chensj@hua-cloud.com.cn>
2022-08-02 21:08:19 +0800

    perf: 移除无用功能

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -2,12 +2,12 @@ import React, { lazy } from 'react';
 import { Navigate, useRoutes, RouteObject } from 'react-router-dom';
 const About = lazy(() => import('@/pages/about'));
 const AboutProcess = lazy(() => import('@/pages/about/pages/process'));
-const Markdown = lazy(() => import('@/pages/markdown'));
-const MarkdownIt = lazy(() => import('@/pages/markdown-it'));
-const RecoilTest = lazy(() => import('@/pages/recoil/index'));
-const Test = lazy(() => import('@/pages/test/index'));
-const VirtualList = lazy(() => import('@/pages/virtuallist'));
-const JsonDownload = lazy(() => import('@/pages/json-download'));
+// const Markdown = lazy(() => import('@/pages/markdown'));
+// const MarkdownIt = lazy(() => import('@/pages/markdown-it'));
+// const RecoilTest = lazy(() => import('@/pages/recoil/index'));
+// const Test = lazy(() => import('@/pages/test/index'));
+// const VirtualList = lazy(() => import('@/pages/virtuallist'));
+// const JsonDownload = lazy(() => import('@/pages/json-download'));
 export interface RouteConfig extends RouteObject {
   name?: string;
 }
@@ -27,36 +27,36 @@ const routes: RouteConfig[] = [
       },
     ],
   },
-  {
-    path: '/test',
-    element: <Test />,
-    name: '测试',
-  },
-  {
-    path: '/markdown',
-    element: <Markdown />,
-    name: 'Markdown',
-  },
-  {
-    path: '/markdown-it',
-    element: <MarkdownIt />,
-    name: 'MarkdownIt',
-  },
-  {
-    path: '/recoil',
-    element: <RecoilTest />,
-    name: 'Recoil',
-  },
-  {
-    path: '/virtuallist',
-    element: <VirtualList />,
-    name: '虚拟长列表',
-  },
-  {
-    path: '/json-download',
-    element: <JsonDownload />,
-    name: 'Json下载',
-  },
+  // {
+  //   path: '/test',
+  //   element: <Test />,
+  //   name: '测试',
+  // },
+  // {
+  //   path: '/markdown',
+  //   element: <Markdown />,
+  //   name: 'Markdown',
+  // },
+  // {
+  //   path: '/markdown-it',
+  //   element: <MarkdownIt />,
+  //   name: 'MarkdownIt',
+  // },
+  // {
+  //   path: '/recoil',
+  //   element: <RecoilTest />,
+  //   name: 'Recoil',
+  // },
+  // {
+  //   path: '/virtuallist',
+  //   element: <VirtualList />,
+  //   name: '虚拟长列表',
+  // },
+  // {
+  //   path: '/json-download',
+  //   element: <JsonDownload />,
+  //   name: 'Json下载',
+  // },
 ];
 
 function RenderRoutes() {
